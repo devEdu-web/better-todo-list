b570a25d84ad0aa223fb8084a392a367b9031ea5
devEdu-web <santos.edu3301@gmail.com>
2022-02-10 14:31:50 -0300

    database implemented

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 require('dotenv').config()
 const app = require('./src/app')
+const db = require('./src/config/database')
 const port = process.env.PORT
 
-app.listen(port, (err) => {
-    if(err) return console.log(err)
 
-    console.log(`Server running on port ${port}`)
+
+db.connectWithMongo()
+.then(result => {
+    app.listen(port, (err) => {
+        if(err) return console.log(err)
+        console.log(`Server running on port ${port}`)
+    })    
 
 })
+.catch(e => console.log(e))
